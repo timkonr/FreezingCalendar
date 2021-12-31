18ef1b29e8af984967df70fc8156d88f5da3c31e
tim <e01267547@student.tuwien.ac.at>
2021-12-31 14:52:36 +0100

    add schwierigkeit modifikator

--- screens/MetronomScreen.tsx
+++ screens/MetronomScreen.tsx
@@ -4,6 +4,9 @@ import {
   AppRegistry,
   Button,
   Keyboard,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
   StyleSheet,
   Text,
   TouchableWithoutFeedback,
@@ -24,12 +27,13 @@ import { Metronome, Tracker } from '../utils';
 
 export const MetronomScreen = ({ navigation }: Props<'Metronom'>) => {
   const [bpm, setBpm] = useState<number>();
+  const [bpmModifier, setBpmModifier] = useState(1);
   const [modalVisible, setModalVisible] = useState(false);
   const [vibrationMode, setVibrationMode] = useState(false);
   const [isTraining, setIsTraining] = useState(false);
   const [metronome, setMetronome] = useState<Metronome>();
   const [tracker, setTracker] = useState<Tracker>();
-  const [radioButtons, setRadioButtons] = useState<RadioButtonProps[]>([
+  const [modusButtons, setModusButtons] = useState<RadioButtonProps[]>([
     {
       id: '1',
       label: 'Sound',
@@ -44,6 +48,35 @@ export const MetronomScreen = ({ navigation }: Props<'Metronom'>) => {
       value: 'vibration',
       onPress: () => setVibrationMode(true),
       disabled: isTraining,
+    },
+  ]);
+  const [schwierigkeitsButtons, setSchwierigkeitsButtons] = useState<
+    RadioButtonProps[]
+  >([
+    {
+      id: '1',
+      label: 'leicht',
+      value: 'leicht',
+      onPress: () => setBpmModifier(0.9),
+      disabled: isTraining,
+      containerStyle: styles.button,
+    },
+    {
+      id: '2',
+      label: 'normal',
+      value: 'normal',
+      selected: true,
+      onPress: () => setBpmModifier(1),
+      disabled: isTraining,
+      containerStyle: styles.button,
+    },
+    {
+      id: '3',
+      label: 'schwer',
+      value: 'schwer',
+      onPress: () => setBpmModifier(1.1),
+      disabled: isTraining,
+      containerStyle: styles.button,
     },
   ]);
 
@@ -63,7 +96,7 @@ export const MetronomScreen = ({ navigation }: Props<'Metronom'>) => {
     if (isTraining) return;
     if (bpm) {
       setIsTraining(true);
-      tracker?.startTracking(bpm);
+      tracker?.startTracking(bpm * bpmModifier);
       if (vibrationMode) {
         const duration = 100;
         const freq = 60000 / bpm;
@@ -100,58 +133,73 @@ export const MetronomScreen = ({ navigation }: Props<'Metronom'>) => {
       }}
     >
       <GameLoop onUpdate={onUpdateHandler}>
-        <View style={styles.screen}>
-          <Card style={styles.inputContainer} title="Cueing Frequenz">
-            <Text style={{ marginBottom: 10, fontWeight: 'bold' }}>
-              {bpm ? bpm + ' bpm' : 'Bitte einstellen'}
-            </Text>
-            <Button
-              color={Colors.primary}
-              title={bpm ? 'Ändern' : 'Einstellen'}
-              onPress={() => {
-                setModalVisible(true);
-              }}
-              disabled={isTraining}
-            />
-          </Card>
-          <FrequencyInput
-            modalVisible={modalVisible}
-            setModalVisible={setModalVisible}
-            setFrequency={setFrequency}
-            cueingFrequency={bpm}
-          />
-          <Card style={styles.inputContainer} title="Modus">
-            <View style={styles.buttonContainer}>
-              <RadioGroup
-                radioButtons={radioButtons}
-                onPress={(radioButtonsArray) =>
-                  setRadioButtons(radioButtonsArray)
-                }
-                layout="row"
-              />
-            </View>
-          </Card>
-          <Card style={styles.inputContainer}>
-            <View style={styles.buttonContainer}>
-              <View style={styles.button}>
+        <SafeAreaView style={styles.container}>
+          <ScrollView style={styles.screen}>
+            <View style={{ alignItems: 'center' }}>
+              <Card style={styles.inputContainer}>
+                <View style={styles.buttonContainer}>
+                  <View style={styles.button}>
+                    <Button
+                      color={Colors.primary}
+                      title="Start"
+                      onPress={startTrainingHandler}
+                      disabled={!bpm || isTraining}
+                    />
+                  </View>
+                  <View style={styles.button}>
+                    <Button
+                      color={Colors.accent}
+                      title="Stopp"
+                      onPress={stopTrainingHandler}
+                      disabled={!bpm || !isTraining}
+                    />
+                  </View>
+                </View>
+              </Card>
+              <Card style={styles.inputContainer} title="Cueing Frequenz">
+                <Text style={{ marginBottom: 10, fontWeight: 'bold' }}>
+                  {bpm ? bpm + ' bpm' : 'Bitte einstellen'}
+                </Text>
                 <Button
                   color={Colors.primary}
-                  title="Start"
-                  onPress={startTrainingHandler}
-                  disabled={!bpm || isTraining}
+                  title={bpm ? 'Ändern' : 'Einstellen'}
+                  onPress={() => {
+                    setModalVisible(true);
+                  }}
+                  disabled={isTraining}
                 />
-              </View>
-              <View style={styles.button}>
-                <Button
-                  color={Colors.accent}
-                  title="Stopp"
-                  onPress={stopTrainingHandler}
-                  disabled={!bpm || !isTraining}
-                />
-              </View>
+              </Card>
+              <FrequencyInput
+                modalVisible={modalVisible}
+                setModalVisible={setModalVisible}
+                setFrequency={setFrequency}
+                cueingFrequency={bpm}
+              />
+              <Card style={styles.inputContainer} title="Modus">
+                <View style={styles.buttonContainer}>
+                  <RadioGroup
+                    radioButtons={modusButtons}
+                    onPress={(radioButtonsArray) =>
+                      setModusButtons(radioButtonsArray)
+                    }
+                    layout="row"
+                  />
+                </View>
+              </Card>
+              <Card style={styles.inputContainer} title="Schwierigkeit">
+                <View style={styles.buttonContainer}>
+                  <RadioGroup
+                    radioButtons={schwierigkeitsButtons}
+                    onPress={(radioButtonsArray) =>
+                      setSchwierigkeitsButtons(radioButtonsArray)
+                    }
+                    layout="column"
+                  />
+                </View>
+              </Card>
             </View>
-          </Card>
-        </View>
+          </ScrollView>
+        </SafeAreaView>
       </GameLoop>
     </TouchableWithoutFeedback>
   );
@@ -160,7 +208,6 @@ export const MetronomScreen = ({ navigation }: Props<'Metronom'>) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    alignItems: 'center',
   },
   title: {
     fontSize: 20,
@@ -180,6 +227,10 @@ const styles = StyleSheet.create({
   },
   button: {
     width: 100,
+  },
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
   },
 });
 
