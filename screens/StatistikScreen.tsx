eb02a47b5d8af2be8b680bd4d59dcfa8dd366476
tim <e01267547@student.tuwien.ac.at>
2022-01-04 01:08:31 +0100

    implement basic trainingtime per day plot

--- screens/StatistikScreen.tsx
+++ screens/StatistikScreen.tsx
@@ -1,20 +1,42 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useCallback, useEffect, useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {
+  VictoryAxis,
+  VictoryBar,
+  VictoryChart,
+  VictoryTheme,
+} from 'victory-native';
 import { TIMES } from '../constants/Values';
 import { Props, TrackedTime } from '../types';
 
-const toTimeString = (ms: number) => {
-  return `${Math.floor(ms / 6000) / 10} Minuten`;
+const toLocaleDateString = (date: string) => {
+  return `${date.slice(3, 5)}.${date.slice(0, 2)}.${date.slice(6, 10)}`;
 };
 
 export const StatistikScreen = ({ navigation }: Props<'Statistik'>) => {
   const [times, setTimes] = useState<TrackedTime[]>();
+  const [yLabels, setYLabels] = useState<number[]>();
 
   const loadTimes = useCallback(async () => {
     const jsonTimes = await AsyncStorage.getItem(TIMES);
-    const times: TrackedTime[] = jsonTimes ? JSON.parse(jsonTimes) : [];
+    const times: TrackedTime[] = jsonTimes
+      ? (JSON.parse(jsonTimes) as TrackedTime[]).map((time) => {
+          return {
+            day: toLocaleDateString(time.day),
+            time: Math.floor(time.time / 6000) / 10,
+          };
+        })
+      : [];
     setTimes(times);
+    const timeArray = times.map((time) => time.time);
+    const max = Math.max(...timeArray);
+    setYLabels([
+      Math.floor((10 * max) / 4) / 10,
+      Math.floor((10 * max * 2) / 4) / 10,
+      Math.floor((10 * max * 3) / 4) / 10,
+      max,
+    ]);
   }, [setTimes]);
 
   useEffect(() => {
@@ -23,15 +45,22 @@ export const StatistikScreen = ({ navigation }: Props<'Statistik'>) => {
 
   return (
     <View style={styles.screen}>
-      <FlatList
-        data={times}
-        keyExtractor={(item) => item.day}
-        renderItem={(item) => (
-          <Text>
-            {item.item.day}: {toTimeString(item.item.time)}
-          </Text>
-        )}
-      />
+      {times && (
+        <VictoryChart
+          domainPadding={20}
+          theme={VictoryTheme.material}
+          padding={100}
+        >
+          <VictoryAxis label="Tag" style={{ axisLabel: { padding: 30 } }} />
+          <VictoryAxis
+            label="Minuten"
+            dependentAxis
+            tickValues={yLabels}
+            style={{ axisLabel: { padding: 50 } }}
+          />
+          <VictoryBar data={times} x="day" y="time" />
+        </VictoryChart>
+      )}
     </View>
   );
 };
